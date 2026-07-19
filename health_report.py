ab1eb6c57be12c79c98dc96af5daff80260ed1ee
Michael Necklen <michaelnecklen@gmail.com>
2026-07-19 09:32:30 -0700

    Add Python version to system report

--- health_report.py
+++ health_report.py
@@ -1,17 +1,20 @@
 from datetime import timedelta
 from pathlib import Path
 import socket
+import platform
 
 
 def get_uptime():
-	uptime_text = Path("/proc/uptime").read_text()
-	uptime_seconds = float(uptime_text.split()[0])
-	return timedelta(seconds=int(uptime_seconds))
+    uptime_text = Path("/proc/uptime").read_text()
+    uptime_seconds = float(uptime_text.split()[0])
+    return timedelta(seconds=int(uptime_seconds))
 
 hostname = socket.gethostname()
+python_version = platform.python_version()
 uptime = get_uptime()
 
 print("Linux System Health Report")
 print("==========================")
-print(f"Hostname: {hostname}")
-print(f"Uptime:   {uptime}")
+print(f"Hostname:  {hostname}")
+print(f"Python:    {python_version}")
+print(f"Uptime:    {uptime}")
